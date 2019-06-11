be474c7b1e79e98592c3106257033ca4f99851b6
Ricardo Mello <ricardo.mello@outlook.com>
2019-06-11 01:57:34 -0300

    hello schematics

---
+++ my-project/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TesteComponent } from './teste/teste.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TesteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
